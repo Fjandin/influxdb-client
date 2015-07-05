7c0e0a91bb44ef01072c4feac8c9348b69db6ffa
René Bischoff <rene.bischoff@gmail.com>
2015-07-05 21:49:20 +0200

    some refactor

--- index.js
+++ index.js
@@ -1,47 +1,7 @@
 "use strict";
 
-var http = require("http");
-var Bluebird = require("bluebird");
 var lodash = require("lodash");
-
-// Helper method to do http requests
-var request = function request(options, body) {
-    // Wrap http in bluebord promise and return
-    return new Bluebird(function(resolve, reject) {
-        var req = http.request(options, function(res) {
-            var chunks = [];
-
-            // Add chunks on data
-            res.on("data", chunks.push.bind(chunks));
-
-            // Join chunks on end and make utf8 string
-            res.on("end", function() {
-                var responseBody = Buffer.concat(chunks).toString("utf8");
-                // Check if response is JSON
-                if (res.headers["content-type"] === "application/json") {
-                    responseBody = JSON.parse(responseBody);
-                }
-                // Add status code and headers
-                resolve({
-                    statusCode: res.statusCode,
-                    headers: res.headers,
-                    body: responseBody
-                });
-            });
-        });
-
-        // Catch errors
-        req.on("error", reject);
-
-        // Write body (if any)
-        if (body) {
-            req.write(body);
-        }
-
-        // End request
-        req.end();
-    });
-};
+var request = require("./lib/request.js");
 
 // Constructor
 var InfluxdbClient = function InfluxConnector(options) {
@@ -54,7 +14,7 @@ var InfluxdbClient = function InfluxConnector(options) {
         user: null,
         password: null,
         retentionPolicy: null,
-        userClientTimestamp: false
+        useClientTimestamp: false
     }, options);
 
     // Store http options template
@@ -74,22 +34,25 @@ var InfluxdbClient = function InfluxConnector(options) {
 InfluxdbClient.prototype.writeRaw = function writeRaw(body, database, retentionPolicy) {
     var options;
 
+    // Retention policy to use
+    retentionPolicy = retentionPolicy || this.options.retentionPolicy || "";
+
     // Http request options
     options = lodash.extend({
         method: "POST",
-        path: "/write?db=" + (database || this.options.database) + "&rp=" + (retentionPolicy || this.options.retentionPolicy || "")
+        path: "/write?db=" + (database || this.options.database) + (retentionPolicy ? "&rp=" + retentionPolicy : "")
     }, this.optionsHttp);
 
     // Do request and return promise
     return request(options, body);
 };
 
-// Send data to influxdb
-InfluxdbClient.prototype.write = function write(name, values, tags, database, retentionPolicy, timestamp) {
+// Parse data point
+InfluxdbClient.prototype.parse = function parse(name, values, tags, timestamp) {
     var body;
 
     // Add name and tags to body
-    body = [name];
+    body = [encodeURIComponent(name)];
 
     // Add tags to body
     body.push(lodash.map(tags || {}, function(v, k) {
@@ -99,7 +62,7 @@ InfluxdbClient.prototype.write = function write(name, values, tags, database, re
     // comma join name and tags
     body = [body.join(",")];
 
-    // Make sure values is an object
+    // Make sure values is an object (default to value)
     if (typeof (values) !== "object") {
         values = {value: values};
     }
@@ -109,29 +72,48 @@ InfluxdbClient.prototype.write = function write(name, values, tags, database, re
         return encodeURIComponent(k) + "=" + encodeURIComponent(v);
     }).join(","));
 
-    // Add timestamp if override timestamp is set
+    // Add timestamp if set
     if (timestamp) {
-        body.push(timestamp * 1000000);
+        body.push(timestamp);
     // Add client timestamp if options it set
-    } else if (this.options.userClientTimestamp) {
-        body.push(Date().now * 1000000);
+    } else if (this.options.useClientTimestamp) {
+        body.push(Date.now());
     }
 
     // Make array and remove double space
     body = body.join(" ").replace(/\s{2,}/g, " ");
 
+    return body;
+};
+
+// Write series data point to Influxdb
+InfluxdbClient.prototype.write = function write(name, values, tags, timestamp, options) {
+    var body;
+
+    // Check if timestamp/options is set
+    if (typeof (timestamp) === "object") {
+        options = timestamp;
+        timestamp = undefined;
+    }
+
+    // Make sure options is an object
+    options = options || {};
+
+    // Parse data point
+    body = this.parse(name, values, tags, timestamp);
+
     // Do request and return promise
-    return this.writeRaw(body, database, retentionPolicy);
+    return this.writeRaw(body, options.database, options.retentionPolicy);
 };
 
 // Query influxdb
-InfluxdbClient.prototype.query = function query(sql, database) {
+InfluxdbClient.prototype.query = function query(ql, database) {
     var options;
 
     // Http request options
     options = lodash.extend({
         method: "GET",
-        path: "/query?db=" + (database || this.options.database) + "&q=" + encodeURIComponent(sql)
+        path: "/query?db=" + (database || this.options.database) + "&q=" + encodeURIComponent(ql)
     }, this.optionsHttp);
 
     // Do request and return promise
